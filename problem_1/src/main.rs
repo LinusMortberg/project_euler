e602f143c511c513a401e9fa7b9bc3206da998b1
Linus Mörtberg <linmort@kth.se>
2023-09-28 21:48:23 +0200

    new file:   src/main.rs

---
+++ problem_1/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut sum: u32 = 0;
+    for i in 0..1000 {
+        if i % 3 == 0 && i & 5 == 0 {
+            sum += i;
+        } else if i % 3 == 0 {
+            sum += i;
+        } else if i % 5 == 0 {
+            sum += i;
+        }
+    }
+    println!("{}", sum)
+}
